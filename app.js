5e4b82fde665bf02daacb2e1e3bbf4afd9a79f94
Claudina Cortez <claudinacr@gmail.com>
2017-10-29 13:33:53 -0300

    Algunos comentarios en el codigo de cambio de color en barra de navegacion

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ function change() {
     /*Para cambiar el color de fondo a blanco cuando el curso*/
     document.getElementById("menu").style.backgroundColor = "white";
 
+    /*Referencia para tomar todos los elementos menubtn*/
     var links = document.getElementsByClassName("menubtn");
 
     for (var i = 0; i < links.length; i++) {
-        if (links[i].href) {
-            links[i].style.color = "black";
-        }
-
+        links[i].style.color = "black";
+        /*Con un bucle cambiamos cada elementos menubtn a letra color negro*/
 
 
     }
+     /*Cambiamos el borde del boton Log in a color negro*/
     document.getElementById("login").style.borderColor = "black";
 }
 
-/*Funcion para volver al icono <logo> inicial*/
+/*Basicamente es la misma funcionalidad de la funcion anterior
+solo que se estaría en este caso cambiando a los colores originales*/
 function changeBack() {
     document.getElementById("logo").src = "assets/images/logo-white.png";
     document.getElementById("menu").style.backgroundColor = "transparent";
@@ -33,13 +34,21 @@ function changeBack() {
     document.getElementById("login").style.borderColor = "white";
 }
 
+/*Funcion para llamar la funcion changeClass cuando el 
+evento window.onscroll se active*/
 window.onscroll = function changeClass() {
     var scrollvalue = 100;
+    
+    /*Referencia de para la posicion del scroll de la ventana del navegador*/
+    
     var scrollPosY = window.pageYOffset | document.body.scrollTop;
+    
+    /* y dependiendo si el scroll de la ventana es mayor a un valor
+    establecido realizamos el cambio de colores a la barra nav*/
     if (scrollPosY > scrollvalue) {
-        change();
+        change(); /*Cambio a color Fondo Blanco*/
     } else if (scrollPosY <= scrollvalue) {
-        changeBack();
+        changeBack(); /*Cambio a color Fondo Transparente*/
     }
 }
 
